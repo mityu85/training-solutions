4ae4114d52dd753660c1230e8b95193cac59d718
mityu85 <mityu85@gmail.com>
2020-10-29 19:30:40 +0100

    changed to English language

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Írj be egy egész számot majd nyomj ENTER-t.");
+        System.out.println("Give a number and press Enter!");
         Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt(); //szám bekérése
+        int num1 = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Írj be egy egész számot majd nyomj ENTER-t.");
+        System.out.println("Give a number and press Enter!");
         int num2 = scanner.nextInt();
         scanner.nextLine();
         System.out.println(num1 + " + " + num2);
