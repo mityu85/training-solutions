c8a5356c399e12fb37e4ed139569d3eceb8108e1
mityu85 <mityu85@gmail.com>
2020-10-29 21:56:39 +0100

    NoteMain first commit

---
+++ src/main/java/classstructuremethods/NoteMain.java
@@ -0,0 +1,11 @@
+package classstructuremethods;
+
+public class NoteMain {
+    public static void main(String[] args) {
+        Note note = new Note();
+        note.setName("Universe");
+        note.setTopic("Milky Way");
+        note.setText("The Big Bang Theory which explains that what is happened in the beginning...");
+        note.getNoteText();
+    }
+}
