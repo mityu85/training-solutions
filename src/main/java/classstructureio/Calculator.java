c9758327546370e9456e1bbf3cb0039e6ed9ed3f
mityu85 <mityu85@gmail.com>
2020-10-29 19:15:01 +0100

    komment

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Írj be egy egész számot majd nyomj ENTER-t.");
         Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
+        int num1 = scanner.nextInt(); //szám bekérése
         scanner.nextLine();
         System.out.println("Írj be egy egész számot majd nyomj ENTER-t.");
         int num2 = scanner.nextInt();
