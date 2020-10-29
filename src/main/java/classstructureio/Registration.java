970641a7760c2f61b96ddbebaf7acb1250d323d7
mityu85 <mityu85@gmail.com>
2020-10-29 19:28:07 +0100

    komment

---
+++ src/main/java/classstructureio/Registration.java
@@ -0,0 +1,16 @@
+package classstructureio;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        System.out.println("What's your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("What's your e-mail address? ");
+        String email = scanner.nextLine();
+        System.out.println("The following personal data have been registered:\n" +
+                "name: " + name + "\n" + "e-mail: " + email);
+    }
+}
