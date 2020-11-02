22603ad71b8c2faaa0410b5550f3fe6f60b76bf1
mityu85 <mityu85@gmail.com>
2020-11-03 00:01:05 +0100

    PerformanceTest first commit

---
+++ src/main/java/introdate/PerformanceTest.java
@@ -0,0 +1,9 @@
+package introdate;
+
+public class PerformanceTest {
+    public static void main(String[] args) {
+
+        Performance p = new Performance();
+        System.out.println(p.getInfo());
+    }
+}
