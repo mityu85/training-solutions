51125c6660d91c68526e189fa7435747d7d8b374
mityu85 <mityu85@gmail.com>
2020-11-03 00:18:27 +0100

    PerformanceTest println modify

--- src/main/java/introdate/PerformanceTest.java
+++ src/main/java/introdate/PerformanceTest.java
@@ -1,9 +1,13 @@
 package introdate;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class PerformanceTest {
     public static void main(String[] args) {
 
-        Performance p = new Performance();
+        Performance p = new Performance(LocalDate.of(1989, 06, 02), "Queen",
+                LocalTime.of(18, 0), LocalTime.of(20, 0));
         System.out.println(p.getInfo());
     }
 }
