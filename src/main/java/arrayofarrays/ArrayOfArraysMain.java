6865b5461910f46d9942266a9b1afccfa04051ed
mityu85 <mityu85@gmail.com>
2020-11-03 19:41:58 +0100

    ArrayOfArraysMain.triangularMatrix commit

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -3,12 +3,14 @@ package arrayofarrays;
 public class ArrayOfArraysMain {
     public static void main(String[] args) {
         ArrayOfArraysMain aoa = new ArrayOfArraysMain();
-        aoa.multiplicationTable(4);
+        /*aoa.multiplicationTable(4);
+        System.out.println();*/
+        aoa.printArrayOfArrays(aoa.multiplicationTable(10));
         System.out.println();
-        aoa.printArrayOfArrays(aoa.multiplicationTable(4));
+        aoa.printArrayOfArrays(aoa.triangularMatrix(10));
     }
 
-        int[][] multiplicationTable(int size) {
+        public int[][] multiplicationTable(int size) {
             int[][] array = new int[size][size];
             for (int i = 0; i < array.length; i++){
                 //System.out.println();
@@ -24,8 +26,27 @@ public class ArrayOfArraysMain {
             for (int[] i : a) {
                 System.out.println();
                 for (int j : i) {
-                    System.out.print(j + " ");
+                    if (j < 10) {
+                        System.out.print(j + "   ");
+                    } else {
+                        if (j < 100) {
+                            System.out.print(j + "  ");
+                        } else {
+                            System.out.print(j + " ");
+                        }
+                    }
                 }
             }
+        }
+
+        public int[][] triangularMatrix(int size) {
+            int[][] array = new int[size][];
+            for (int i = 0; i < array.length; i++){
+                array[i] = new int[i+1];
+                for (int j = 0; j < i+1; j++) {
+                    array[i][j] = i+1;
+                }
+            }
+            return array;
         }
     }
