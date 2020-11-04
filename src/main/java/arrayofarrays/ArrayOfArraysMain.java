a158b213df0618a7fd83d25c4b471c825bc08995
mityu85 <mityu85@gmail.com>
2020-11-04 14:43:04 +0100

    ArrayOfArraysMain.getValues commit

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -8,6 +8,8 @@ public class ArrayOfArraysMain {
         aoa.printArrayOfArrays(aoa.multiplicationTable(10));
         System.out.println();
         aoa.printArrayOfArrays(aoa.triangularMatrix(10));
+        System.out.println();
+        aoa.printArrayOfArrays(aoa.getValues());
     }
 
         public int[][] multiplicationTable(int size) {
@@ -45,6 +47,25 @@ public class ArrayOfArraysMain {
                 array[i] = new int[i+1];
                 for (int j = 0; j < i+1; j++) {
                     array[i][j] = i+1;
+                }
+            }
+            return array;
+        }
+
+        public int[][] getValues() {
+            int[][] array = new int[12][];
+            for (int i = 0; i < array.length; i++) {
+                if (i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11) {
+                    array[i] = new int[31];
+                } else {
+                    if (i != 1) {
+                        array[i] = new int[30];
+                    } else {
+                        array[1] = new int[28];
+                    }
+                }
+                for (int j = 0; j < array[i].length; j++) {
+                    array[i][j] = j+1;
                 }
             }
             return array;
