8fe26edd7070bde39a6568b5b036ec3931bd96f9
mityu85 <mityu85@gmail.com>
2020-11-04 20:12:37 +0100

    Books first commit

---
+++ src/main/java/arraylist/Books.java
@@ -0,0 +1,36 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books {
+
+    public static void main(String[] args) {
+        Books books = new Books();
+        books.add("Kincskereső Kisködmön");
+        books.add("Köszívű ember fiai");
+        books.add("Egri Cillagok");
+        System.out.println(books.getTitles());
+        System.out.println(books.findAllByPrefix("Kincs"));
+    }
+
+    private List<String> l = new ArrayList<>();
+
+    public void add(String s) {
+        l.add(s);
+    }
+
+    List<String> findAllByPrefix(String prefix) {
+        List<String> f = new ArrayList<>();
+        for (String item: l) {
+            if (item.startsWith(prefix)) {
+                f.add(item);
+            }
+        }
+        return f;
+    }
+
+    public List<String> getTitles() {
+        return l;
+    }
+ }
