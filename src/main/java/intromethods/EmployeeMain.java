99a68239bbf460fc10e773ab23df051a37507add
mityu85 <mityu85@gmail.com>
2020-11-08 10:23:59 +0100

    EmployeeMain first commit

---
+++ src/main/java/intromethods/EmployeeMain.java
@@ -0,0 +1,13 @@
+package intromethods;
+
+public class EmployeeMain {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("John Doe", 2010, 450000);
+        System.out.println(employee);
+
+        employee.setName("Jane Doe");
+        System.out.println(employee);
+    }
+}
