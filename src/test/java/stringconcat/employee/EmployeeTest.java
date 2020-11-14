d634ec27ab4d1841ad7459b757b8ce5a859b32ec
mityu85 <mityu85@gmail.com>
2020-11-14 10:22:00 +0100

    EmployeeTest commit

--- src/test/java/stringconcat/employee/EmployeeTest.java
+++ src/test/java/stringconcat/employee/EmployeeTest.java
@@ -1,6 +1,5 @@
-package employee;
+package stringconcat.employee;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
