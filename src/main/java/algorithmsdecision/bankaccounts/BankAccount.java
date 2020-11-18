90a7a57fb6c84dfa768e94153e8452a62bf72240
mityu85 <mityu85@gmail.com>
2020-11-18 16:59:51 +0100

    BankAccount add Constructor and Getters

--- src/main/java/algorithmsdecision/bankaccounts/BankAccount.java
+++ src/main/java/algorithmsdecision/bankaccounts/BankAccount.java
@@ -8,6 +8,24 @@ public class BankAccount {
     private String accountNumber;
     private int balance;
 
+    public BankAccount(String nameOfOwner, String accountNumber, int balance) {
+        this.nameOfOwner = nameOfOwner;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getNameOfOwner() {
+        return nameOfOwner;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
     public void withdraw(int value) {
         balance -= value;
     }
