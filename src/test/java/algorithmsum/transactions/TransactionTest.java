88874c191bc2e9555fd3f9b99cd2db464651c0fd
mityu85 <mityu85@gmail.com>
2020-11-18 10:06:56 +0100

    TransactionTest first commit

---
+++ src/test/java/algorithmsum/transactions/TransactionTest.java
@@ -0,0 +1,19 @@
+package algorithmsum.transactions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TransactionTest {
+
+    @Test
+    public void testCreateTransaction() {
+        Transaction transaction = new Transaction("1234-2345-1231", TransactionOperation.CREDIT, 100000);
+
+        assertEquals(100000, transaction.getAmount());
+        assertTrue(transaction.isCredit());
+        assertFalse(transaction.isDebit());
+    }
+}
