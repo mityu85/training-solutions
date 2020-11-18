c7c08eb031227407b68b92d4788812f4531f7911
mityu85 <mityu85@gmail.com>
2020-11-18 10:07:41 +0100

    SalesPerson first commit

---
+++ src/main/java/algorithmsum/sales/SalesPerson.java
@@ -0,0 +1,20 @@
+package algorithmsum.sales;
+
+public class SalesPerson {
+
+    private String name;
+    private int amount;
+
+    public SalesPerson(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
