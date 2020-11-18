11c367375c5d1985c703b84c9d02bbea7a860026
mityu85 <mityu85@gmail.com>
2020-11-18 21:08:57 +0100

    Mark first commit

---
+++ src/main/java/schoolrecords/Mark.java
@@ -0,0 +1,36 @@
+package schoolrecords;
+
+public class Mark {
+
+    private MarkType markType;
+    private Subject subject;
+    private Tutor tutor;
+
+    public Mark(MarkType markType, Subject subject, Tutor tutor) {
+        this.markType = markType;
+        this.subject = subject;
+        this.tutor = tutor;
+    }
+
+    public Mark(String markType, Subject subject, Tutor tutor) {
+        this.markType = MarkType.valueOf(markType);
+        this.subject = subject;
+        this.tutor = tutor;
+    }
+
+    public MarkType getMarkType() {
+        return markType;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public String toString() {
+        return markType.getDescription() + "(" + markType.getValue() +")";
+    }
+}
