f2171908498425c265bb64bfc8bfc422313d0d1e
mityu85 <mityu85@gmail.com>
2020-11-27 06:12:55 +0100

    Product is modified with currency

--- src/main/java/week05d04/Product.java
+++ src/main/java/week05d04/Product.java
@@ -6,6 +6,8 @@ public class Product {
 
     private String name;
     private LocalDate expirationDate;
+    private long price;
+    private Currency currency;
 
     public Product(String name, int year, int month, int day) {
 
@@ -13,11 +15,37 @@ public class Product {
         expirationDate = LocalDate.of(year, month, day);
     }
 
+    public Product(long price, Currency currency) {
+            this.price = price;
+            this.currency = currency;
+        }
+
     public String getName() {
         return name;
     }
 
     public LocalDate getExpirationDate() {
         return expirationDate;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public double convertPrice(Currency currency) {
+        double value = 0.0;
+        if (currency.equals(Currency.HUF) && this.currency.equals(Currency.HUF)) {
+            value = price;
+        }
+        if (currency.equals(Currency.HUF) && this.currency.equals(Currency.USD)) {
+            value = price * 300.0;
+        }
+        if (currency.equals(Currency.USD) && this.currency.equals(Currency.USD)) {
+            value = price;
+        }
+        if (currency.equals(Currency.USD) && this.currency.equals(Currency.HUF)) {
+            value = price / 300.0;
+        }
+        return value;
     }
 }
