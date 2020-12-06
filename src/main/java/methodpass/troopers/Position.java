1574e26f59b8e6c0eee29d2165cfd4bc9d9fe228
mityu85 <mityu85@gmail.com>
2020-12-06 20:29:21 +0100

    Position first commit

---
+++ src/main/java/methodpass/troopers/Position.java
@@ -0,0 +1,24 @@
+package methodpass.troopers;
+
+public class Position {
+
+    private double posX;
+    private double posY;
+
+    public Position(double posX, double posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+    public double distanceFrom(Position position) {
+        return Math.sqrt(Math.pow(posX - position.posX, 2) + Math.pow(posY - position.posY, 2));
+    }
+}
