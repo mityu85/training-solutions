a7b0383cc28ed54eff3ac8266346f0ccb0dfef5a
mityu85 <mityu85@gmail.com>
2020-12-10 22:30:14 +0100

    Beer first commit

---
+++ src/main/java/objectclass/Beer.java
@@ -0,0 +1,40 @@
+package objectclass;
+
+import java.util.Objects;
+
+public class Beer {
+
+    private String name;
+    private int price;
+
+    public Beer(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Beer beer = (Beer) o;
+
+        if (price != beer.price) return false;
+        return name != null ? name.equals(beer.name) : beer.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + price;
+        return result;
+    }
+}
