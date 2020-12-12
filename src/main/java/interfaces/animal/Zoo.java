a8f2bbc984308c287243354c4543220c06b1ecaa
mityu85 <mityu85@gmail.com>
2020-12-12 21:38:30 +0100

    Zoo modified commit

--- src/main/java/interfaces/animal/Zoo.java
+++ src/main/java/interfaces/animal/Zoo.java
@@ -7,10 +7,6 @@ public class Zoo {
 
     private List<Animal> animals;
 
-    private Animal duck = new Duck();
-    private Animal lion = new Lion();
-    private Animal worm = new Worm();
-
     public Zoo(List<Animal> animals) {
         this.animals = animals;
     }
