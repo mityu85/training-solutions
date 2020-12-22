b4a32741aaf3c3183846a66791dd0bbc3db667d8
mityu85 <mityu85@gmail.com>
2020-12-22 22:28:46 +0100

    PrintedFeatures first commit

---
+++ src/main/java/catalog/PrintedFeatures.java
@@ -0,0 +1,39 @@
+package catalog;
+
+import java.util.List;
+
+public class PrintedFeatures implements Feature {
+
+    private List<String> authors;
+    private int numberOfPages;
+    private String title;
+
+    public PrintedFeatures(String title, int numberOfPages, List<String> authors) {
+        if (Validators.isBlank(title)) {
+            throw new IllegalArgumentException("Empty title");
+        }
+        if (numberOfPages <= 0) {
+            throw new IllegalArgumentException();
+        }
+        if (Validators.isEmpty(authors)) {
+            throw new IllegalArgumentException();
+        }
+        this.authors = authors;
+        this.numberOfPages = numberOfPages;
+        this.title = title;
+    }
+
+    @Override
+    public List<String> getContributors() {
+        return authors;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+}
