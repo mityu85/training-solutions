d885f8c4eff4921ba2f87596e495e8e469e47c7a
mityu85 <mityu85@gmail.com>
2020-12-31 16:51:05 +0100

    LotteryMain first commit

---
+++ src/main/java/week08d02/LotteryMain.java
@@ -0,0 +1,9 @@
+package week08d02;
+
+public class LotteryMain {
+
+    public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        System.out.println(lottery.getNumbers());
+    }
+}
