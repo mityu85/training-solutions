a8cab74633de3f05fe862739ff709d15f02b7a3a
mityu85 <mityu85@gmail.com>
2020-12-31 16:49:35 +0100

    LotteryTest first commit

---
+++ src/test/java/week08d02/LotteryTest.java
@@ -0,0 +1,14 @@
+package week08d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LotteryTest {
+
+    @Test
+    public void testLottery() {
+        Lottery lottery = new Lottery();
+        assertEquals(5, lottery.getNumbers().size());
+    }
+}
