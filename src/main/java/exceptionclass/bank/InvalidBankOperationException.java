55dd2c4486323721936cb82f438a0af6ff2d2cb2
mityu85 <mityu85@gmail.com>
2021-01-01 19:24:57 +0100

    InvalidBankOperationException first commit

---
+++ src/main/java/exceptionclass/bank/InvalidBankOperationException.java
@@ -0,0 +1,10 @@
+package exceptionclass.bank;
+
+public class InvalidBankOperationException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public InvalidBankOperationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
