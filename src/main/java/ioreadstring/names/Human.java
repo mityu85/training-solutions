f2739c8a5cb23445be4a315f43c99311cfac43d5
mityu85 <mityu85@gmail.com>
2021-01-02 15:16:55 +0100

    Human first commit

---
+++ src/main/java/ioreadstring/names/Human.java
@@ -0,0 +1,20 @@
+package ioreadstring.names;
+
+public class Human {
+
+    private String firstname;
+    private String lastname;
+
+    public Human(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public String getLastName() {
+        return lastname;
+    }
+}
