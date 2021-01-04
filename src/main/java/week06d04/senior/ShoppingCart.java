ba188452cb6d536ee524a62038d08da835fc5b35
mityu85 <mityu85@gmail.com>
2021-01-04 23:49:36 +0100

    ShoppingCart first commit

---
+++ src/main/java/week06d04/senior/ShoppingCart.java
@@ -0,0 +1,36 @@
+package week06d04.senior;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+
+    private List<Item> items = new ArrayList<>();
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void addItem(String name, int quantity) {
+        List<Item> itemList = new ArrayList<>();
+        for (Item item: items) {
+            if (name.equals(item.getName())) {
+               item.addQuantity(quantity);
+               itemList.add(item);
+            }
+        }
+        if (itemList.size() == 0) {
+            items.add(new Item(name, quantity));
+        }
+    }
+
+    public int getItem(String name) {
+        int quantity = 0;
+        for (Item item: items) {
+            if (name.equals(item.getName())) {
+               quantity = item.getQuantity();
+            }
+        }
+        return quantity;
+    }
+}
