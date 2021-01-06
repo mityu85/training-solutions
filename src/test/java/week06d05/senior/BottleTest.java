12d2da01b71ad6b35b35c6cfbf68527148d693ca
mityu85 <mityu85@gmail.com>
2021-01-06 14:44:15 +0100

    BottleTest throws refactor

--- src/test/java/week06d05/senior/BottleTest.java
+++ src/test/java/week06d05/senior/BottleTest.java
@@ -1,5 +1,7 @@
 package week06d05.senior;
 
+import immutable.satellite.CelestialCoordinates;
+import immutable.satellite.Satellite;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,8 +17,12 @@ public class BottleTest {
     public void testFill() {
         petBottle.fill(15);
         assertEquals(15, petBottle.getFilledUntil());
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> petBottle.fill(100));
+        assertEquals("The Bottle is full", ex.getMessage());
         glassBottle.fill(42);
         assertEquals(42, glassBottle.getFilledUntil());
+        Exception ex2 = assertThrows(IllegalArgumentException.class, () -> glassBottle.fill(100));
+        assertEquals("The Bottle is full", ex2.getMessage());
     }
 
 }
