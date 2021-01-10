c95c8e609d4ffa51d2173df7f4322e8c5b0ff513
mityu85 <mityu85@gmail.com>
2021-01-10 18:32:15 +0100

    Stop first commit

---
+++ src/main/java/week11d01/Stop.java
@@ -0,0 +1,20 @@
+package week11d01;
+
+public class Stop {
+
+    private int number;
+    private int numberOfPersons;
+
+    public Stop(int number, int numberOfPersons) {
+        this.number = number;
+        this.numberOfPersons = numberOfPersons;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getNumberOfPersons() {
+        return numberOfPersons;
+    }
+}
