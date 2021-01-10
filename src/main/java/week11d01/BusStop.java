32f85df56458b6f4e054e2b8280d2f69ca0f4ea9
mityu85 <mityu85@gmail.com>
2021-01-10 18:32:01 +0100

    BusStop first commit

---
+++ src/main/java/week11d01/BusStop.java
@@ -0,0 +1,38 @@
+package week11d01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusStop {
+
+    private List<Stop> stops = new ArrayList<>();
+
+    private void collectDatas(List<Integer> integers) {
+        int sum = 1;
+        for (int i = 0; i < integers.size()-1; i++) {
+            if (integers.get(i) == integers.get(i+1)) {
+                sum++;
+            } else {
+                stops.add(new Stop(integers.get(i), sum));
+                sum = 1;
+            }
+            stops.add(new Stop(integers.get(i), sum));
+            /*if (integers.get(i+1) != integers.get(i)) {
+                stops.add(new Stop(integers.get(i), sum));
+            }*/
+        }
+    }
+
+    public int getMaxIndex(List<Integer> integers) {
+        collectDatas(integers);
+        int stopNumber = 0;
+        int maxIndex = Integer.MIN_VALUE;
+        for (Stop stop: stops) {
+            if (stop.getNumberOfPersons() > maxIndex) {
+                maxIndex = stop.getNumberOfPersons();
+                stopNumber = stop.getNumber();
+            }
+        }
+        return stopNumber;
+    }
+}
