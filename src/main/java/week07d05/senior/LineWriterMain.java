b2db6ab1831fbbea265c14b5bec4ba422cbd962c
mityu85 <mityu85@gmail.com>
2021-01-10 15:32:38 +0100

    LineWriterMain first commit

---
+++ src/main/java/week07d05/senior/LineWriterMain.java
@@ -0,0 +1,9 @@
+package week07d05.senior;
+
+public class LineWriterMain {
+
+    public static void main(String[] args) {
+        LineWriter lineWriter = new LineWriter();
+        lineWriter.lineWriter();
+    }
+}
