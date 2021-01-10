59a8da09fed318d14c3acdf332b8cda941e4d964
mityu85 <mityu85@gmail.com>
2021-01-10 16:26:38 +0100

    SantaClaus first commit

---
+++ src/main/java/week09d03/SantaClaus.java
@@ -0,0 +1,18 @@
+package week09d03;
+
+import java.util.List;
+
+public class SantaClaus {
+
+    private List<Person> people;
+
+    public SantaClaus(List<Person> people) {
+        this.people = people;
+    }
+
+    public void getThroughChimneys() {
+        for (Person person: people) {
+            person.setPresent();
+        }
+    }
+}
