61e8b76d639e990f58716f1777e0351e3199ff76
mityu85 <Taxi2000!>
2021-01-17 21:12:39 +0100

    WordFilterTest first commit

---
+++ src/test/java/week12d03/WordFilterTest.java
@@ -0,0 +1,20 @@
+package week12d03;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class WordFilterTest {
+
+    WordFilter wf = new WordFilter();
+
+    @Test
+    public void testWordFilter() {
+        List<String> testWords = List.of("apple", "pear", "grape", "banana");
+        assertEquals(1, wf.wordsWithChar(testWords, 'n').size());
+        assertEquals(4, wf.wordsWithChar(testWords, 'a').size());
+    }
+}
