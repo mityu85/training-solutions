24c53b3699a793a1b7e7948cf21725c4d4dbd361
mityu85 <Taxi2000!>
2021-01-17 20:35:39 +0100

    DivisorFinder first commit

---
+++ src/main/java/week12d01/DivisorFinder.java
@@ -0,0 +1,23 @@
+package week12d01;
+
+public class DivisorFinder {
+
+    public int findDivisors(int n) {
+        String number = Integer.toString(n);
+        int length = number.length();
+        int[] temp = new int[length];
+        int divisorCounter = 0;
+        int remainder = 0;
+        for (int i = 0; i < length; i++) {
+            remainder = n % 10;
+            n = n / 10;
+            temp[i] = remainder;
+        }
+        for (int i = 0; i < temp.length; i++) {
+            if ((Integer.parseInt(number) % temp[i]) == 0) {
+                divisorCounter++;
+            }
+        }
+        return divisorCounter;
+    }
+}
