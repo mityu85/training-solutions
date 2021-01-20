50a3bb5e067a16dd3c7dcb21b18c67308815ebb6
mityu85 <Taxi2000!>
2021-01-20 22:02:19 +0100

    countries first commit

---
+++ src/main/java/ioreaderclasspath/countries/Country.java
@@ -0,0 +1,20 @@
+package ioreaderclasspath.countries;
+
+public class Country {
+
+    private String name;
+    private int number;
+
+    public Country(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
---
+++ src/main/java/ioreaderclasspath/countries/CountryStatistics.java
@@ -0,0 +1,55 @@
+package ioreaderclasspath.countries;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryStatistics {
+
+    private List<Country> countries = new ArrayList<>();
+
+    public List<Country> getCountries() {
+        return new ArrayList<>(countries);
+    }
+
+    public void readCountriesFromFileToList() {
+        try (BufferedReader bf = new BufferedReader(new InputStreamReader(
+                CountryStatistics.class.getResourceAsStream("/country.txt")
+        ))) {
+            String line;
+            while ((line = bf.readLine()) != null) {
+                String[] temp = line.split(" ");
+                countries.add(new Country(temp[0], Integer.parseInt(temp[1])));
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("File can not found", e);
+        }
+    }
+
+    public int howManyCountriesHaveBeenRead() {
+        readCountriesFromFileToList();
+        int sum = 0;
+        for (Country country: countries) {
+            sum++;
+        }
+        return sum;
+    }
+
+    public Country whichCountryHaveTheMostNeighbor() {
+        int num = 0;
+        Country c = new Country(" ", 0);
+        for (Country country: countries) {
+            if (country.getNumber() > num) {
+                num = country.getNumber();
+            }
+        }
+        for (Country country: countries) {
+            if (country.getNumber() == num) {
+                c = country;
+            }
+        }
+        return c;
+    }
+}
