aee9e0023c4a13a795f9a93fbe436314f6d7657d
mityu85 <Taxi2000!>
2021-01-20 22:02:04 +0100

    countries test first commit

---
+++ src/test/java/ioreaderclasspath/countries/CountryStatisticsTest.java
@@ -0,0 +1,50 @@
+package ioreaderclasspath.countries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CountryStatisticsTest {
+
+    private CountryStatistics countryStatistics = new CountryStatistics();
+
+
+    @Test
+    public void createListTest() {
+        assertEquals(0, countryStatistics.getCountries().size());
+
+        countryStatistics.getCountries().add(new Country("Test", 3));
+
+        assertEquals(0, countryStatistics.getCountries().size());
+    }
+
+
+    @Test
+    public void readFromFileTest() {
+        assertEquals(countryStatistics.getCountries().size(), 0);
+        countryStatistics.readCountriesFromFileToList();
+
+        assertEquals(8, countryStatistics.getCountries().size());
+
+        assertEquals("Austria", countryStatistics.getCountries().get(1).getName());
+        assertEquals(1, countryStatistics.getCountries().get(5).getNumber());
+    }
+
+    @Test
+    public void numberOfCountriesTest() {
+    //    assertEquals(0, countryStatistics.howManyCountriesHaveBeenRead());
+    //    countryStatistics.readCountriesFromFileToList();
+
+        assertEquals(8, countryStatistics.howManyCountriesHaveBeenRead());
+    }
+
+
+    @Test
+    public void mostBorderCountriesTest() {
+        countryStatistics.readCountriesFromFileToList();
+
+        assertEquals("Germany", countryStatistics.whichCountryHaveTheMostNeighbor().getName());
+        assertEquals(8, countryStatistics.whichCountryHaveTheMostNeighbor().getNumber());
+
+    }
+}
