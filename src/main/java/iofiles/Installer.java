277d91398516dacb7ad9c99f720072d9fe5008f2
mityu85 <Taxi2000!>
2021-01-23 18:46:12 +0100

    Installer first commit

---
+++ src/main/java/iofiles/Installer.java
@@ -0,0 +1,32 @@
+package iofiles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+
+public class Installer {
+
+    public void installer(String folder) {
+        final String SOURCE_PATH = "/install/";
+        Path path = Path.of(folder);
+        if ( (!Files.exists(path)) || (!Files.isDirectory(path)) ) {
+            throw new IllegalStateException("Folder is not exist");
+        }
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/install/install.txt")))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.endsWith("/")) {
+                    Files.createDirectories(path.resolve(line));
+                } else {
+                    Files.copy(this.getClass().getResourceAsStream("/install/" + line), path.resolve(line), StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("File can not read", e);
+        }
+    }
+}
