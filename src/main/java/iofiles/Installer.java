f85181e278e1ec9bc87e67a5075845dade8f6cd3
mityu85 <Taxi2000!>
2021-01-23 18:53:20 +0100

    Installer first commit

--- src/main/java/iofiles/Installer.java
+++ src/main/java/iofiles/Installer.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
 
 public class Installer {
 
