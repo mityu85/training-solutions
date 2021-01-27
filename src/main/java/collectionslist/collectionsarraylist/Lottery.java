e9421ed0bbcfc6eb09bf15d2a8456c26b750edb2
mityu85 <Taxi2000!>
2021-01-27 20:45:18 +0100

    Lottery first commit

---
+++ src/main/java/collectionslist/collectionsarraylist/Lottery.java
@@ -0,0 +1,21 @@
+package collectionslist.collectionsarraylist;
+
+import java.util.*;
+
+public class Lottery {
+
+    public List<Integer> selectWinningNumbers(int lotteryType, int ballCount) {
+
+        if (ballCount <= lotteryType) {
+            throw new IllegalArgumentException("Balls must be more then the winning numbers!");
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 1; i <= lotteryType; i++) {
+            numbers.add(rand.nextInt(ballCount-1)+1);
+        }
+        numbers.sort(Comparator.naturalOrder());
+        return numbers;
+    }
+}
