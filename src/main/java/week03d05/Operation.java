b2ae05ac52a37ce18242fc20f88dbb1ee0cff46a
mityu85 <Taxi2000!>
2021-03-14 10:16:44 +0100

    week03d04 first commit

---
+++ src/main/java/week03d05/Operation.java
@@ -0,0 +1,24 @@
+package week03d05;
+
+public class Operation {
+
+    private int leftOperand;
+    private int rightOperand;
+
+    public Operation(int leftOperand, int rightOperand) {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+    }
+
+    public int getLeftOperand() {
+        return leftOperand;
+    }
+
+    public int getRightOperand() {
+        return rightOperand;
+    }
+
+    public int getResult() {
+        return leftOperand + rightOperand;
+    }
+}
---
+++ src/main/java/week03d05/OperationMain.java
@@ -0,0 +1,9 @@
+package week03d05;
+
+public class OperationMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Operation(68, 12).getResult());
+    }
+}
